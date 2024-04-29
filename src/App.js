967551afe9c66f5394cce7e868967448e9fd0f81
Andy-Warhurst <andy@andywarhurst.co.uk>
2024-04-30 08:57:40 +0930

    Removed (commented out) learn react link

--- src/App.js
+++ src/App.js
@@ -13,15 +13,15 @@ function App() {
         </p>
         <LoginButton></LoginButton><br/>
         <LogoutButton></LogoutButton>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
 
-          Learn React
-        </a>
+        {/*  Learn React*/}
+        {/*</a>*/}
       </header>
     </div>
   );
