fe40f9772dcda9c100106da9cdaf68e7041a655c
Andy-Warhurst <andy@andywarhurst.co.uk>
2024-05-01 17:42:47 +0930

    Made Login and Logout buttons exclusive, and made them look sexy.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 function Home() {
+  const { isAuthenticated } = useAuth0();
+
   return (
       <div>
         <header className="App-header">
@@ -12,17 +16,19 @@ function Home() {
           <p>
             Welcome to the SAMSL Manager Website
           </p>
-          <LoginButton></LoginButton><br/>
-          <LogoutButton></LogoutButton>
-          {/*<a*/}
-          {/*  className="App-link"*/}
-          {/*  href="https://reactjs.org"*/}
-          {/*  target="_blank"*/}
-          {/*  rel="noopener noreferrer"*/}
-          {/*>*/}
 
-          {/*  Learn React*/}
-          {/*</a>*/}
+
+          {!isAuthenticated && (
+              <>
+                <LoginButton></LoginButton>
+              </>
+          )}
+          {isAuthenticated && (
+            <>
+              <LogoutButton />
+            </>
+        )}
+
           <Link to="/about">About</Link>
         </header>
       </div>
@@ -33,13 +39,13 @@ function About() {
   return (
       <div>
         <h1>About Page</h1>
-        <p>
-          Version: 0.1
-        </p>
-        <p>
-          Date: 1 May 2024
-        </p>
-        <Link to="/">Home</Link>
+              <p>
+                Version: 0.1
+              </p>
+              <p>
+                Date: 1 May 2024
+              </p>
+              <Link to="/">Home</Link>
       </div>
   );
 
