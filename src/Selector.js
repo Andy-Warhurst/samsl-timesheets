9b902df3a05ab9bebf7ecc44b7d36d309aa5ccd9
Andy-Warhurst <andy@andywarhurst.co.uk>
2024-06-18 22:34:18 +0930

    Optimisations in Selector.js

--- src/Selector.js
+++ src/Selector.js
@@ -1,5 +1,5 @@
 import "./Selector.css"
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import FormControl from "react-bootstrap/FormControl";
 import {useData} from "./DataContext";
 import {usePlayers} from "./PlayerContext";
@@ -19,74 +19,56 @@ const Selector = (props) => {
         setMyPlayers(players.filter(extractPlayersByTeam).sort((a, b) => a.name > b.name));
     }, [players, props.team]);
 
-    //const players = allPlayers.filter(extractPlayersByTeam).sort((a, b) => a.name > b.name);
+    const updateSelected = useCallback((selection) => {
+        const updatedSelections = data.selectedPlayers.includes(selection)
+            ? data.selectedPlayers.filter(player => player !== selection)
+            : [...data.selectedPlayers, selection];
+
+        updateUserField('selectedPlayers', updatedSelections);
+    }, [data.selectedPlayers, updateUserField]);
+
+    const updateShirtNumber = useCallback((player, number) => {
+        updatePlayer({ ...player, shirtno: number });
+    },[updatePlayer]);  //
 
 
-
-  function updateSelected (selection) {
-
-    var updatedSelections = [];
-    updatedSelections = updatedSelections.concat(data.selectedPlayers);
-
-    if (!updatedSelections.includes(selection)) {
-      updatedSelections.push(selection);
-    } else {
-      var index = updatedSelections.indexOf(selection);
-      updatedSelections.splice(index, 1);
-    }
-
-    updateUserField('selectedPlayers', updatedSelections);
-  }
-
-  function updateShirtNumber(player, number) {
-    player.shirtno = number;
-    updatePlayer (player);
-  }
-
-
-
-  return (
-
-        // <div className="player-container">
-
-      <table className="player-selector-table">
-        <tbody>
-        <tr>
-          <th>Select</th>
-          <th>Name</th>
-          <th>Shirt</th>
-        </tr>
-        {myPlayers.map(p => (
-            <tr key={p.id}>
-                <td>
-                    <input
-                        className="player-checkbox"
-                        type="checkbox"
-                        id={"playing".concat(p.id)}
-                        size="20"
-                        onClick={() => updateSelected(p)}
-                    />
-                </td>
-                <td className="player-name" align='left'>{p.name}</td>
-                <td width='50'>
-                    <FormControl
-                        className={"player-shirt"}
-                        id={"shirtnumber".concat(p.id)}
-                        name={"shirtnumber".concat(p.id)}
-                        type="text"
-                        value={p.shirtno}
-                        onChange={(e) =>
-                            updateShirtNumber(p, e.target.value)}
-                    />
-                </td>
+    return (
+        <table className="player-selector-table">
+            <tbody>
+            <tr>
+                <th>Select</th>
+                <th>Name</th>
+                <th>Shirt</th>
             </tr>
-        ))}
+            {myPlayers.map(p => (
+                <tr key={p.id}>
+                    <td>
+                        <input
+                            className="player-checkbox"
+                            type="checkbox"
+                            id={"playing".concat(p.id)}
+                            size="20"
+                            onClick={() => updateSelected(p)}
+                        />
+                    </td>
+                    <td className="player-name" align='left'>{p.name}</td>
+                    <td width='50'>
+                        <FormControl
+                            className={"player-shirt"}
+                            id={"shirtnumber".concat(p.id)}
+                            name={"shirtnumber".concat(p.id)}
+                            type="text"
+                            value={p.shirtno}
+                            onChange={(e) =>
+                                updateShirtNumber(p, e.target.value)}
+                        />
+                    </td>
+                </tr>
+            ))}
 
-        </tbody>
-      </table>
-
-
-);
+            </tbody>
+        </table>
+    );
 
 };
 
