66c0f629f1c48efea7344580422b5ef46ad96132
andy <git@andywarhurst.co.uk>
2024-07-05 09:58:05 +0930

    Include cup rounds in FixtureDropdown.js

--- src/About.js
+++ src/About.js
@@ -8,10 +8,10 @@ function About() {
         <div>
             <h1>About Page</h1>
             <p>
-                Version: 1.0
+                Version: 1.01
             </p>
             <p>
-                Date: 21 June 2024
+                Date: 5 July 2024
             </p>
             <Link to="/">Home</Link>
         </div>
